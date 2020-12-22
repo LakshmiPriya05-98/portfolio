446cc1d18c46c163ee9b72490c43cb4276770283
priya <69449035+LakshmiPriya05-98@users.noreply.github.com>
2020-12-22 17:53:39 +0530

    create

---
+++ src/contents/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Contact Me</h1>
+            <h3>Email       :priya05kannan@gmail.com</h3>
+            <h3>Phoneno     :6385846002</h3>
+            <h3>Instagram   :Lavanyapriya05</h3>
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
